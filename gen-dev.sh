badb31f98adceffcd060b4a8e9ceba1d6526c854
Daniel Pernold <daniel.pernold@gmail.com>
2018-09-14 13:54:49 +0200

    Initial commit

---
+++ gen-dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+openssl genrsa -out dev.key 2048
+openssl req -x509 -new -nodes -key dev.key -sha256 -days 1825 -out dev.pem
+openssl req -new -key dev.key -out dev.csr
+openssl x509 -req -in dev.csr -CA dev.pem -CAkey dev.key -CAcreateserial -out dev.crt -days 365 -sha256 -extfile dev.ext
+
+echo "
+###############################################################
+Configure your webserver to use the 'dev.crt' as certificate
+and 'dev.key' as certificate-key.
+Here's an example for Nginx:
+    ssl_certificate /path/to/your/certs/dev.crt;
+    ssl_certificate_key /path/to/your/certs/dev.key;
+Do not forget to import 'dev.pem' in your browser as Authority.
+In Chrome for examle navigate to:
+    Settings > Advanced > Manage certificates > Authorities
+###############################################################
+"
